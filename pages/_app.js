e05dccd36dbc5bff2f57b6262e59763bc71b7c9a
Your name <raphael075@hotmail.fr>
2024-12-16 15:57:51 +0100

    persistore

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,36 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import render from '../reducers/render';
 import user from '../reducers/user';
 import city from '../reducers/city';
 
+
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+
+const reducers = combineReducers({ render, user, city });
+const persistConfig = { key: 'applicationName', storage };
+
+
 const store = configureStore({
- reducer: {render, user, city},
-});
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+ });
+
+ const persistor = persistStore(store);
 
 function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Head>
-        <title>Next.js App</title>
-      </Head>
-      <Component {...pageProps} />
+      <PersistGate persistor={persistor}>
+            <Head>
+              <title>Next.js App</title>
+            </Head>
+          <Component {...pageProps} />
+        </PersistGate>
       </Provider>
   );
 }
